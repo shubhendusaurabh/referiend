b2511b87acc18f304d2e439a17481657ce3186f4
Shubhendu <shubhendu.saurabh@hotmail.com>
2014-07-01 14:52:23 +0530

    removed frequency and priority attributes from referral sitemaps

--- referrals/sitemaps.py
+++ referrals/sitemaps.py
@@ -3,8 +3,6 @@ from django.contrib.sitemaps import Sitemap, FlatPageSitemap
 from .models import Referral
 
 class ReferralSitemap(Sitemap):
-    changefreq = "never"
-    priority = 0.5
 
     def items(self):
         return Referral.objects.all()
